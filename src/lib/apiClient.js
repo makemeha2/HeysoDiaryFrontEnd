282b8f91d84871d276d52d9b1dae953fb16b8336
soyoung <makemeha2@gmail.com>
2026-02-10 19:12:59 +0900

    배포관련 코드 수정

--- src/lib/apiClient.js
+++ src/lib/apiClient.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = import.meta.env.VITE_API_BASE_URL || '';
+const baseUrl = '';
 
 export function getAuthData() {
   try {
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import './index.css'
-import App from './App.jsx'
-import { GoogleOAuthProvider } from "@react-oauth/google";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import './index.css';
+import App from './App.jsx';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 const queryClient = new QueryClient();
@@ -18,5 +18,5 @@ createRoot(document.getElementById('root')).render(
         </QueryClientProvider>
       </GoogleOAuthProvider>
     </BrowserRouter>
-  </StrictMode>
-)
+  </StrictMode>,
+);
